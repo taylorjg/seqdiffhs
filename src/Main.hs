03a548dad687773a98845b5bb1d9b32a76e01d95
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-22 00:31:00 +0100

    We now print each Diff on a new line

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 import           SeqDiff
 
 main :: IO ()
-main = putStrLn $ show ds
+main = mapM_ putStrLn $ map show ds
     where
-        xs = [0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 11, 12]
         ds = diffs xs
+        xs = [0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 11, 12]
