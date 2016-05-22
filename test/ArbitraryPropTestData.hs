ea56b240bed09c96bc4e52b7662f592a7537f4aa
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-22 23:21:52 +0100

    Got a working Gen PropTestData

--- test/ArbitraryPropTestData.hs
+++ test/ArbitraryPropTestData.hs
@@ -2,9 +2,37 @@ module ArbitraryPropTestData () where
 
 import Test.QuickCheck
 import PropTestData
+import SeqDiff
+import Data.List (sortOn)
+import Data.Bits (shiftR)
 
 genPropTestData :: Gen PropTestData
-genPropTestData = return $ PropTestData 10 [] []
+genPropTestData = do
+    len <- genSequenceLength
+    ds <- genDuplicateDiffs len
+    ms <- genMissingDiffs len
+    let ptd = PropTestData {len = len, ds = ds, ms = ms}
+    return ptd
+
+genSequenceLength :: Gen Int
+genSequenceLength = choose (0, 100)
+
+genDuplicateDiffs :: Int -> Gen [Diff]
+genDuplicateDiffs = genDiffs Duplicate
+
+genMissingDiffs :: Int -> Gen [Diff]
+genMissingDiffs = genDiffs Missing
+
+genDiffs :: DiffType -> Int -> Gen [Diff]
+genDiffs dt n = do
+    let lastValue = n - 1
+    let tenthOfN = floor $ (realToFrac n) / 10
+    numValues <- choose (0, tenthOfN)
+    values <- vectorOf numValues $ choose (0, lastValue)
+    counts <- vectorOf numValues $ choose (1, 5)
+    let vcs = zip values counts
+    let ds = map (\(v, c) -> Diff {diffType = dt, value = v, count = c}) vcs
+    return $ sortOn (\d -> (value d)) ds
 
 shrinkPropTestData :: PropTestData -> [PropTestData]
 shrinkPropTestData ptd = []
--- test/PropertyTests.hs
+++ test/PropertyTests.hs
@@ -10,11 +10,8 @@ prop_PropTestData _ = True
 
 main :: IO ()
 main = do
-    let args = stdArgs {chatty = True, maxSuccess = 500}
+    let args = stdArgs {chatty = True, maxSuccess = 5}
     let tests = [verboseCheckWithResult args prop_PropTestData]
-    let ptd = PropTestData 13 [Diff {diffType = Duplicate, value = 4, count = 3}] [Diff {diffType = Missing, value = 9, count = 2}]
-    let xs = toList ptd
-    putStrLn $ show xs
     results <- sequence tests
     if all isSuccess results
         then return ()
