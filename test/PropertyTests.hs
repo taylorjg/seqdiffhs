9b1801396aef9e42bc125831ea2b3632e6aeee65
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-22 12:46:06 +0100

    Using exitFailure to ensure that test suite failure is recognised

--- test/PropertyTests.hs
+++ test/PropertyTests.hs
@@ -1,6 +1,7 @@
 import Test.QuickCheck
 import Test.QuickCheck.Test
 import SeqDiff
+import System.Exit (exitFailure)
 
 prop_True1 :: Int -> Bool
 prop_True1 _ = True
@@ -11,5 +12,8 @@ prop_True2 n = if n > 10 then False else True
 main :: IO ()
 main = do
     let args = stdArgs {chatty = True, maxSuccess = 500}
-    quickCheckWith args prop_True1
-    quickCheckWith args prop_True2
+    r1 <- quickCheckWithResult args prop_True1
+    r2 <- quickCheckWithResult args prop_True2
+    if all isSuccess [r1, r2]
+        then return ()
+        else exitFailure
