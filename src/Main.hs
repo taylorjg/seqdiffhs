a16fe910a7b5df3e4e7252662b10c35484cd3b47
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-22 11:52:43 +0100

    Main now prints xs too

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 import           SeqDiff
 
 main :: IO ()
-main = mapM_ putStrLn $ map show ds
+main = do
+    putStrLn $ show xs
+    mapM_ putStrLn $ map show ds
     where
         ds = diffs xs
         xs = [0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 11, 12]
