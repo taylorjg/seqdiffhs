8c6777de00115efa6ab1388630709fcc83d40193
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-05-22 01:08:01 +0100

    Tweaks

--- src/SeqDiff.hs
+++ src/SeqDiff.hs
@@ -16,24 +16,24 @@ diffs xs = loop xs expectedValue repeatCount
 loop :: [Int] -> Int -> Int -> [Diff]
 loop xs ev rc =
     case xs of
-        v:vs | v == ev - 1 -> loop vs ev (rc + 1)
+        v:vs | v == (pred ev) ->
+            loop vs ev (succ rc)
         v:vs ->
             let
-                d1 = if (rc > 0) then Just (Diff {diffType = Duplicate, value = ev - 1, count = rc}) else Nothing
-                d2 = if (v > ev) then Just (Diff {diffType = Missing, value = ev, count = v - ev}) else Nothing
-                ds = loop vs ev' rc'
-                ev' = v + 1
-                rc' = 0
+                d1 = if (rc > 0) then Just $ makeDuplicate (pred ev) rc else Nothing
+                d2 = if (v > ev) then Just $ makeMissing ev (v - ev) else Nothing
+                ds = loop vs (succ v) 0
             in
                 case (d1, d2) of
                     (Just d1, Just d2) -> d1 : d2 : ds
                     (Just d1, Nothing) -> d1 : ds
                     (Nothing, Just d2) -> d2 : ds
                     (Nothing, Nothing) -> ds
-        _ ->
-            let
-                d = if (rc > 0) then Just (Diff {diffType = Duplicate, value = ev - 1, count = rc}) else Nothing
-            in
-                case d of
-                    Just d -> [d]
-                    Nothing -> []
+        [] ->
+            if (rc > 0) then return $ makeDuplicate (pred ev) rc else []
+
+makeDuplicate :: Int -> Int -> Diff
+makeDuplicate v c = Diff {diffType = Duplicate, value = v, count = c}
+
+makeMissing :: Int -> Int -> Diff
+makeMissing v c = Diff {diffType = Missing, value = v, count = c}
